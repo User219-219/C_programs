e58f43f3f5215b5e684d2de6f1cf6ff4a77acb68
User219-219 <152054736+User219-219@users.noreply.github.com>
2026-09-11 00:44:08 +0300

    Add files via upload
    A C++ program  that displays all the academic units offered the first semester at Zetech University

---
+++ Praise_academic_units.cpp
@@ -0,0 +1,18 @@
+/* Author:Kennedy Derrick Praise
+   Registration_number:BCS-03-0139/2026
+   Description:A C++ program that displays all the academic units offered the first semester at Zetech universith
+   Date:2026-09-11
+   Version:1.0 */
+   #include <stdio.h>
+   int main() {
+    //displaying the academic units offered in the first semester at Zetech university
+    printf("Units offered in the first semester at Zetech university:\n");
+    printf("1.Basic Mathematics\n");
+    printf("2.Computational Logic\n");
+    printf("3.Communication Skills\n");
+    printf("4.Introduction to Computers and Internet\n");
+    printf("5.Health And Wellness Management\n");
+    printf("6.Structured Programming And Algorithms\n");
+    printf("7.Mathematical Physics\n");
+    return 0; //execution successful
+   }
